f783926d682feb71744d18c045757a386a3730ee
volak-klara <klaravolak@gmail.com>
2025-12-10 12:44:02 +0100

    Add Firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,17 @@ import 'models/recipe.dart';
 import 'screens/recipe_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+}
+
 
 void main () async {
 
@@ -12,6 +23,22 @@ void main () async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // NotificationSettings settings =
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+    // provisional: true,
+  );
+
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('FCM token: $fcmToken');
+
   runApp(MyApp());
 }
 
@@ -33,6 +60,31 @@ class MyHomePage extends StatefulWidget {
 
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      final notification = message.notification;
+      if (notification != null) {
+        print('Foreground notification: ${notification.title} - ${notification.body}');
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+      final recipe = await ApiService().getRandomRecipe();
+      if (!mounted) return;
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => RecipeScreen(mealId: recipe.id),
+        ),
+      );
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
